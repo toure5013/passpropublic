78e12c3ae44d27bc850d1ebd62d9edab4ad07f04
toure5013 <toure5013@gmail.com>
2024-12-02 18:15:34 +0000

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { configService } from '../providers/configService';
 
 interface LogoProps {
   className?: string;
@@ -6,14 +7,16 @@ interface LogoProps {
 }
 
 export default function Logo({ className = "h-8", style = {} }: LogoProps) {
+  const logo = configService.baseUrlImage + "/event_cover/1XEVENTTEST_cover_2024_12_02_16_15_29.PNG"
   return (
     <img 
-      src="/assets/images/logo 1.svg" 
+      src={logo} 
       alt="PassPro" 
       className={className}
       style={{ 
         objectFit: 'contain',
         objectPosition: 'left',
+        height: '100px',
         ...style 
       }}
     />
